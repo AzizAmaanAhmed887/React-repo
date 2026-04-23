7269b65496851ee51b47900d36031030e4a48adf
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2026-04-24 00:06:12 +0530

    add initial implementation of comments feature with form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import CommentsForm from "./CommentsForm"
 import Comment from './Comment.jsx';
+import {Counter} from "./Counter.jsx";
 
 export default function App() {
     return (
         <>
-            <Comment />
+            <Counter/>
         </>
     )
 }
