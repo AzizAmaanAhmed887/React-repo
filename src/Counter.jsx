cfec838f1d42273e6b2ca19823a2e99f5e89acf3
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2026-04-24 14:16:33 +0530

    add initial implementation of comments feature with form validation

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,26 @@
 import {useEffect, useState} from "react";
 
 export function Counter() {
-    let [count, setCount] = useState(0);
+    let [countX, setCountX] = useState(0);
+    let [countY, setCountY] = useState(0);
 
-    let increment = () => {
-        setCount(currCount => currCount + 1);
+    let incrementX = () => {
+        setCountX(currCount => currCount + 1);
+    }
+    let incrementY = () => {
+        setCountY(currCount => currCount + 1);
     }
 
     useEffect(() => {
-        console.log("Count has been updated to " + count);
-    })
-
+        console.log("This is side-effect");
+    }, [countY]);
     return (
         <>
-            <h3>Count = {count}</h3>
-            <button onClick={increment}>+1</button>
+            <h3>Count = {countX}</h3>
+            <button onClick={incrementX}>+1</button>
+
+            <h3>Count = {countY}</h3>
+            <button onClick={incrementY}>+1</button>
         </>
     )
 }
